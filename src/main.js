42453d8665bfb78e42fd994d8c82d8fabfee23c8
cxcxy <897939354@qq.com>
2018-08-08 14:47:30 +0800

    引入 vuex

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import Vuex from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css' // 引入 element-ui 样式表
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
